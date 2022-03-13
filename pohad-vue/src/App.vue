eb0965b20ec452d7ee7b2deec89a41b4bdca4368
pohadkove <80869110+pohadkove@users.noreply.github.com>
2022-03-13 11:48:43 -0700

    vue

--- pohad-vue/src/App.vue
+++ pohad-vue/src/App.vue
@@ -2,17 +2,25 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import btn from './components/clicks-btn.vue'
-
+import vuetify from './plugins/vuetify.js'
 </script>
 
 <style>
 @import './assets/base.css';
-
 </style>
 <script>
 
 </script>
 
+
 <template>
-   <btn class="btn"></btn>
+<br>
+
+<br>
+
+    <v-btn variant="outlined">outlined</v-btn>
+  
+ 
+
+    
 </template>
