49f8d6444631c87c16e5dff96a16def9f592c75a
pohadkove <80869110+pohadkove@users.noreply.github.com>
2022-03-12 23:03:56 -0800

    vue

---
+++ pohad-vue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import btn from './components/clicks-btn.vue'
+
+</script>
+
+<style>
+@import './assets/base.css';
+
+</style>
+<script>
+
+</script>
+
+<template>
+   <btn class="btn"></btn>
+</template>
---
+++ pohad-vue/src/components/clicks-btn.vue
@@ -0,0 +1,53 @@
+<template>
+<button class="ripple" @click="increment">Clicks {{ count }}</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+button {
+  position: relative;
+  overflow: hidden;
+  -webkit-transition: background 400ms;
+  transition: background 400ms;
+  color: #fff;
+  background-color: #6200ee;
+  padding: 1em 2rem;
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.5rem;
+  outline: 0;
+  border: 0;
+  border-radius: 0.25rem;
+  -webkit-box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.3);
+  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+}
+.ripple {
+  background-position: center;
+  -webkit-transition: background 0.8s;
+  transition: background 0.8s;
+}
+.ripple:hover {
+  background: #6200ee radial-gradient(circle, transparent 1%, #6200ee 1%) center/15000%;
+}
+.ripple:active {
+  background-color: #cfb2f9;
+  background-size: 100%;
+  -webkit-transition: background 0s;
+  transition: background 0s;
+}
+</style>
