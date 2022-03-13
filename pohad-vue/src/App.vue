2a0b45a86eb7db4c079f1e3eee5051128894813a
pohadkove <80869110+pohadkove@users.noreply.github.com>
2022-03-13 12:32:24 -0700

    1

--- pohad-vue/src/App.vue
+++ pohad-vue/src/App.vue
@@ -1,8 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import btn from './components/clicks-btn.vue'
 import vuetify from './plugins/vuetify.js'
+
 </script>
 
 <style>
@@ -15,12 +14,22 @@ import vuetify from './plugins/vuetify.js'
 
 <template>
 <br>
+<header class="text-gray-400 bg-dark body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+         <p> <v-btn variant="text"> <v-icon large color="" >mdi-bow-tie </v-icon> Pohadkove</v-btn></p>
+          <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+            <a href="Discord.html" class="mr-5 hover:text-white">Gon Bot</a>
+            
+            <a href="pastel.html" class="mr-5 hover:text-white">pastel.css</a>
+          </nav>
 
+          <a href="http://localhost:3000/">
+               <v-btn variant="outlined">Home</v-btn>
+  </a>
+
+        </div>
+      </header>
 <br>
-
-    <v-btn variant="outlined">outlined</v-btn>
-  
- 
 
     
 </template>
