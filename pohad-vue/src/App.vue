d4639a18e54a0a338a6546e2c8fd9c1810acbdc5
pohadkove <80869110+pohadkove@users.noreply.github.com>
2022-03-13 13:11:54 -0700

    1

--- pohad-vue/src/App.vue
+++ pohad-vue/src/App.vue
@@ -16,15 +16,15 @@ import vuetify from './plugins/vuetify.js'
 <br>
 <header class="text-gray-400 bg-dark body-font">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-         <p> <v-btn variant="text"> <v-icon large color="" >mdi-bow-tie </v-icon> Pohadkove</v-btn></p>
+         <p> <v-btn variant="text"> <v-icon large color="secondary" >mdi-bow-tie </v-icon> Pohadkove</v-btn></p>
           <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-            <a href="Discord.html" class="mr-5 hover:text-white">Gon Bot</a>
+            <a href="Discord.html" class="mr-5 hover:text-white"><v-btn variant="text">Gon Bot</v-btn></a>
             
-            <a href="pastel.html" class="mr-5 hover:text-white">pastel.css</a>
+            <a href="pastel.html" class="mr-5 hover:text-white"><v-btn variant="text">pastel.css</v-btn></a>
           </nav>
 
           <a href="http://localhost:3000/">
-               <v-btn variant="outlined">Home</v-btn>
+               <v-btn variant="outlined" color="secondary">Home</v-btn>
   </a>
 
         </div>
